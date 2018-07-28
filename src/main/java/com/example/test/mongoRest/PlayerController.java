dc8daa5cbacecaea882511a9ef17c889c241330c
Justin  Shotsberger <jas1785@Justins-MacBook-Pro.local>
2018-07-27 20:33:43 -0400

    basics of player controller working with mongo now

--- src/main/java/com/example/test/mongoRest/PlayerController.java
+++ src/main/java/com/example/test/mongoRest/PlayerController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,30 +24,38 @@ public class PlayerController {
 	  @Autowired
 	  private PlayersRepository repository;
 	  
+	  @CrossOrigin
 	  @RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	  public Players getPlayerById(@PathVariable("id") ObjectId id) {
 	    return repository.findBy_id(id);
 	  }
+	  
+	  @CrossOrigin
 	  @RequestMapping(value = "/", method = RequestMethod.GET)
 	  public List<Players> getAllPlayers() {
+		 for(Players a:repository.findAll()) {
+			 a.set_id(a.get_id());
+			 System.out.println(a.toString());
+		 } 
 	    return repository.findAll();
 	  }
 	  
+	  @CrossOrigin
 	  @RequestMapping(value = "/create/", method = RequestMethod.POST)
 	  public Players createPlayer(@Valid @RequestBody Players player) {
 		player.set_id(ObjectId.get());
-	    return repository.save(player);
+	    return repository.insert(player);
 	  }
 	  
+	  @CrossOrigin
 	  @RequestMapping(value = "/update/", method = RequestMethod.PUT)
 	  public Players updatePlayer(@Valid @RequestBody Players player) {
 	    return repository.save(player);
 	  }
 	  
+	  @CrossOrigin
 	  @RequestMapping(value = "/delete/", method = RequestMethod.DELETE)
 	  public void deletePlayer(@Valid @RequestBody Players player) {
 	     repository.delete(player);
 	  }
-
-
 }
--- src/main/java/com/tracker/model/Players.java
+++ src/main/java/com/tracker/model/Players.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
 public class Players {
 	
 	  @Id
 	  public ObjectId _id;
-	  
+
 	  public int number;
 	  public String firstName;
 	  public String lastName;
@@ -46,5 +48,19 @@ public class Players {
 	public void setGames(List<Game> games) {
 		this.games = games;
 	}
-	  
+	
+	/**
+	 * Gets the _id of the object and returns the hex string value.
+	 * This value will be used to find players by there object id
+	 * @return
+	 */
+	public String getObjectIdHexString() {  
+		return this._id.toHexString();
+	}
+	@Override
+	public String toString() {
+		return "Players [_id=" + _id + ", number=" + number + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", games=" + games + ", getObjectIdHexString()=" + getObjectIdHexString() + "]";
+	}
+	
 }
